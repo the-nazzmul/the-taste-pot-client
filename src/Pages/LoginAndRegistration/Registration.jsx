860c0cdde630f55301b5ef53b6cf76b5d031e759
Nazmul Hussain <nazmul.hussain.utchash@gmail.com>
2023-06-09 01:02:53 +0600

    added error handling to register

--- src/Pages/LoginAndRegistration/Registration.jsx
+++ src/Pages/LoginAndRegistration/Registration.jsx
@@ -25,19 +25,19 @@ const Registration = () => {
                 })
                     .then(res => res.json())
                     .then(imageRes => {
-                        if(imageRes.success){
+                        if (imageRes.success) {
                             data.image = imageRes.data.display_url;
                             updateUserProfile(data.name, data.image)
-                            .then(()=>{
-                                Swal.fire({
-                                    position: 'center',
-                                    icon: 'success',
-                                    title: 'Registration Successful!',
-                                    showConfirmButton: false,
-                                    timer: 1500
-                                  })
-                                navigate('/')
-                            })
+                                .then(() => {
+                                    Swal.fire({
+                                        position: 'center',
+                                        icon: 'success',
+                                        title: 'Registration Successful!',
+                                        showConfirmButton: false,
+                                        timer: 1500
+                                    })
+                                    navigate('/')
+                                })
                         }
                     })
             })
@@ -61,12 +61,14 @@ const Registration = () => {
                                     <span className="label-text">Name</span>
                                 </label>
                                 <input type="text" placeholder="name" className="input input-bordered" {...register('name', { required: true })} />
+                                {errors.name && <p className="text-red-600">Name is required</p>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Email</span>
                                 </label>
                                 <input type="text" placeholder="email" className="input input-bordered" {...register('email', { required: true })} />
+                                {errors.email && <p className="text-red-600">Email is required</p>}
                             </div>
 
 
@@ -74,7 +76,16 @@ const Registration = () => {
                                 <label className="label">
                                     <span className="label-text">Password</span>
                                 </label>
-                                <input type="password" placeholder="password" className="input input-bordered" {...register('password', { required: true })} />
+                                <input type="password" placeholder="password" className="input input-bordered" {...register('password', {
+                                    required: true,
+                                    minLength: 6,
+                                    maxLength: 20,
+                                    pattern: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z])/
+                                })} />
+                                {errors.password?.type === 'required' && <p className="text-red-600">Password is required</p>}
+                                {errors.password?.type === "minLength" && <p className="text-red-600">Password must be at least 6 characters</p>}
+                                {errors.password?.type === 'maxLength' && <p className="text-red-600">Password must not exceed 20 characters</p>}
+                                {errors.password?.type === 'pattern' && <p className="text-red-600">Password must have one uppercase letter, one lowercase letter, one special character and one number</p>}
                             </div>
                             <div className="form-control">
                                 <label className="label">
@@ -88,7 +99,7 @@ const Registration = () => {
                                         }
                                     },
                                 })} />
-                                <p className="text-red-600">{errors.confirm_password?.message}</p>
+                                {errors.confirm_password && <p className="text-red-600">Both passwords must match</p>}
                             </div>
 
                             <div className="form-control w-full max-w-xs">
@@ -96,13 +107,14 @@ const Registration = () => {
                                     <span className="label-text">Upload Image</span>
                                 </label>
                                 <input type="file" className="file-input file-input-bordered w-full max-w-xs" {...register('image', { required: true })} />
+                                {errors.image && <p className="text-red-600">Must Upload image</p>}
                             </div>
 
 
                             <div className="form-control my-6">
                                 <input className="custom-btn" type="submit" value="Register" />
                             </div>
-                            <small>Already Have an account? <Link to='/login' className="text-red-600">Login</Link></small>
+                            <p>Already Have an account? <Link to='/login' className="text-red-600">Login</Link></p>
                         </form>
                     </div>
                 </div>
