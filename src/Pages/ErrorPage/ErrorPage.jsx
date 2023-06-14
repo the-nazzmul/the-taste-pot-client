f6f417aa71468d9535c9644384bdd3119fb29edb
Nazmul Hussain <nazmul.hussain.utchash@gmail.com>
2023-06-14 18:38:01 +0600

    added 404 page

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,15 +1,33 @@
-import { MdOutlineError } from "react-icons/md";
+import { Helmet } from "react-helmet";
 import { Link, useRouteError } from 'react-router-dom';
+// import './styles.css';
+import Lottie from 'react-lottie';
+import animationData from './../../assets/Lottie/error.json';
 
 const ErrorPage = () => {
 
     const { error, status } = useRouteError()
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice"
+        }
+    };
     return (
-        <div className='flex flex-col justify-center items-center h-screen text-center py-32'>
-           <MdOutlineError className='text-red-300 text-9xl'/>
+        <div className='container flex flex-col justify-center items-center h-screen text-center py-32'>
+            <Helmet>
+                <title>Error</title>
+            </Helmet>
+            <Lottie
+                options={defaultOptions}
+                height={400}
+                width={400}
+            />
             <h1 className=' text-7xl font-extrabold mb-8'>Error {status || 404}</h1>
-            <p className='text-5xl'>{error?.message}</p>
-            <button className='btn bg-indigo-600  border-0 mt-8'>
+            <p className='lg:text-3xl'>{error?.message}</p>
+            <button className='custom-btn mt-8'>
                 <Link to='/'>Go Back to HomePage</Link>
             </button>
         </div>
