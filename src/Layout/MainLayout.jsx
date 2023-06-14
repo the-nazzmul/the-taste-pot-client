07cf785f7395f846a1f96e4a6e9e996aa2a0c4bb
Nazmul Hussain <nazmul.hussain.utchash@gmail.com>
2023-06-14 21:45:14 +0600

    final push

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import NavBar from "../Components/NavBar";
 import { Outlet } from "react-router-dom";
 import Footer from "../Components/Footer";
@@ -6,28 +5,15 @@ import Footer from "../Components/Footer";
 
 
 const MainLayout = () => {
-    const [darkMode, setDarkMode] = useState(false)
 
-    // const handleModeToggle = () => {
-    //     setDarkMode(!darkMode)
-    // }
 
     return (
-        <div className={`${darkMode ? 'dark bg-grey-500 text-white' : 'bg-white'}  h-screen`}>
-
+        <div>
             <NavBar></NavBar>
             <div className="min-h-screen">
                 <Outlet></Outlet>
             </div>
             <Footer></Footer>
-
-
-
-            {/* {
-                darkMode ?
-                    <button onClick={handleModeToggle}>Dark</button> :
-                    <button onClick={handleModeToggle}>Light</button>
-            } */}
         </div>
     );
 };
