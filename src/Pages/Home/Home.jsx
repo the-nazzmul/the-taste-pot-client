20e568f568d6e3dfb681c8fc9449bd687689aa24
Nazmul Hussain <nazmul.hussain.utchash@gmail.com>
2023-06-14 20:24:34 +0600

    added react awesome reveal

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import { Helmet } from "react-helmet";
 import Banner from "../../Components/Banner";
 import PopularCourses from "./PopularCourses";
 import PopularInstructors from "./PopularInstructors";
-
+import { Fade } from "react-awesome-reveal";
 
 const Home = () => {
     return (
@@ -11,8 +11,8 @@ const Home = () => {
                 <title>The Taste Pot | Home</title>
             </Helmet>
             <Banner></Banner>
-            <PopularCourses></PopularCourses>
-            <PopularInstructors></PopularInstructors>
+            <Fade><PopularCourses></PopularCourses></Fade>
+            <Fade><PopularInstructors></PopularInstructors></Fade>
         </div>
     );
 };
