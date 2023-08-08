456a677a30729be621f70ae503b52195ec0464d6
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2023-08-08 21:13:14 +0600

    extension

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,105 +1,106 @@
 import { Link, NavLink } from "react-router-dom";
-import logo from './../assets/logo.svg'
+import logo from "./../assets/logo.svg";
 import useAuth from "../Hooks/useAuth";
 import useUserRole from "../Hooks/useUserRole";
-import { useEffect, useState } from "react";
-import { BsMoonStarsFill, BsSunFill } from "react-icons/bs";
-
 
 const NavBar = () => {
-    // const [theme, setTheme] = useState(
-    //     localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
-    // );
-
-    // const handleToggle = (e) => {
-    //     if (e.target.checked) {
-    //         setTheme("dark");
-    //     } else {
-    //         setTheme("light");
-    //     }
-    // };
-
-    // useEffect(() => {
-    //     localStorage.setItem("theme", theme);
-    //     const localTheme = localStorage.getItem("theme");
-    //     document.querySelector("html").setAttribute("data-theme", localTheme);
-    // }, [theme]);
-
-    const { user, logOut } = useAuth()
-    const [userRole] = useUserRole()
-    const navigationElement = <>
-        <li>
-            <div className="lg:hidden avatar mr-2">
-                <div className="w-14 border-4 border-white rounded-full">
-                    <img src={user?.photoURL} />
-                </div>
-            </div>
-        </li>
-        <li><NavLink to='/'>Home</NavLink></li>
-        <li><NavLink to='/instructors'>Instructors</NavLink></li>
-        <li><NavLink to='/courses'>Courses</NavLink></li>
-        {
-            userRole === 'student' && <li><NavLink to='/dashboard/selectedClasses'>Dashboard</NavLink></li>
-        }
-        {
-            userRole === 'instructor' && <li><NavLink to='/dashboard/myClasses'>Dashboard</NavLink></li>
-        }
-        {
-            userRole === 'admin' && <li><NavLink to='/dashboard/manageUsers'>Dashboard</NavLink></li>
-        }
-
-    </>
-
-    const handleLogOut = () => {
-        logOut()
-    }
-    return (
-        <div>
-            <div className="navbar bg-[#FDF7EC] fixed z-10">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            {navigationElement}
-                        </ul>
-                    </div>
-                    <Link to='/'><img className="w-[200px]" src={logo} alt="" /></Link>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        {navigationElement}
-                    </ul>
-                </div>
-                <div className="navbar-end">
-                    {
-                        user ?
-                            <>
-                                <div className=" hidden lg:block avatar mr-2">
-                                    <div className="w-14 border-4 border-white rounded-full">
-                                        <img src={user?.photoURL} />
-                                    </div>
-                                </div>
-                                <button onClick={handleLogOut} className="custom-btn">Logout</button>
-                            </> :
-                            <Link to='/login'><button className="custom-btn">Login</button></Link>
-                    }
-                    {/* <button className="btn btn-md btn-circle btn-ghost ml-2">
-                        <label className="swap swap-rotate w-12 h-12">
-                            <input
-                                type="checkbox"
-                                onChange={handleToggle}
-                                checked={theme === "light" ? false : true}
-                            />
-                            <BsSunFill alt="light" className="w-8 h-8 swap-on text-orange-400" />
-                            <BsMoonStarsFill alt="dark" className="w-8 h-8 swap-off text-blue-950" />
-                        </label>
-                    </button> */}
-                </div>
-            </div>
+  const { user, logOut } = useAuth();
+  const [userRole] = useUserRole();
+  const navigationElement = (
+    <>
+      <li>
+        <div className="lg:hidden avatar mr-2">
+          <div className="w-14 border-4 border-white rounded-full">
+            <img src={user?.photoURL} />
+          </div>
         </div>
-    );
+      </li>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+      <li>
+        <NavLink to="/instructors">Instructors</NavLink>
+      </li>
+      <li>
+        <NavLink to="/courses">Courses</NavLink>
+      </li>
+      {userRole === "student" && (
+        <li>
+          <NavLink to="/dashboard/selectedClasses">Dashboard</NavLink>
+        </li>
+      )}
+      {userRole === "instructor" && (
+        <li>
+          <NavLink to="/dashboard/myClasses">Dashboard</NavLink>
+        </li>
+      )}
+      {userRole === "admin" && (
+        <li>
+          <NavLink to="/dashboard/manageUsers">Dashboard</NavLink>
+        </li>
+      )}
+    </>
+  );
+
+  const handleLogOut = () => {
+    logOut();
+  };
+  return (
+    <div>
+      <div className="navbar bg-[#FDF7EC] fixed z-40">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navigationElement}
+            </ul>
+          </div>
+          <Link to="/">
+            <img className="w-[200px]" src={logo} alt="" />
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navigationElement}</ul>
+        </div>
+        <div className="navbar-end">
+          {user ? (
+            <>
+              <div className=" hidden lg:block avatar mr-2">
+                <div className="w-14 border-4 border-white rounded-full">
+                  <img src={user?.photoURL} />
+                </div>
+              </div>
+              <button onClick={handleLogOut} className="custom-btn">
+                Logout
+              </button>
+            </>
+          ) : (
+            <Link to="/login">
+              <button className="custom-btn">Login</button>
+            </Link>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
--- src/Components/SubBanner.jsx
+++ src/Components/SubBanner.jsx
@@ -3,10 +3,10 @@ const SubBanner = () => {
     <div className="text-center sub-bg relative">
         <div className="bg-black absolute opacity-60 w-full h-full"></div>
       <div className="flex flex-col items-center justify-center p-20 absolute w-full h-full z-20 text-white">
-        <h1 className="text-4xl font-bold">
+        <h1 className="text-2xl lg:text-4xl font-bold">
           Age specific courses
         </h1>
-        <p className="my-12 w-3/4 mx-auto">In Taste Pot, we acknowledge that learning ability depends on age many times. So we are offering age specific cooking courses to provide optimal learning environment to the students.</p>
+        <p className="my-4 lg:my-12 lg:w-3/4 mx-auto">In Taste Pot, we acknowledge that learning ability depends on age many times. So we are offering age specific cooking courses to provide optimal learning environment to the students.</p>
         <button className="btn custom-btn">Check Courses</button>
       </div>
     </div>
